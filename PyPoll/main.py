94365da70da93d6d3941de57a12ec82d3117f261
lizbetheli <lizbeth.eli2326@gmail.com>
2020-07-07 15:43:11 -0600

    Final ready eddited spacing

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,8 +8,6 @@ csvpath= os.path.join("Resources", "election_data.csv")
 with open (csvpath, newline = '') as csvfile:
     csvreader=csv.reader(csvfile,delimiter=',')
     csv_header=next(csvreader)
-    
-
 
     #variables
     candidates = {} 
